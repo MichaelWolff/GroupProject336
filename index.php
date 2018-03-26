3f8dbc4992d83a7df8fd8acda34c4386b16802b9
Michael Wolff <mwolff@csumb.edu>
2018-03-26 15:49:28 +0000

    The most recent changes with the third array for motherboards

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include 'inc/functions.php';
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,7 +18,7 @@ alert(info);
 </script>
     </head>
     <body>
-        <h2>Device Checkout</h2>
+        <h2>PC Parts Store</h2>
         <form action="index.php" method="post">
           Model:<br>
           <input type="text" name="Model"><br>
@@ -32,7 +33,7 @@ alert(info);
             ///////////////////////////////////////////////////////////////////////
             //This is the example provided on ilearn modified to fit this project
             
-            
+        
         $orderCPUBy = array('Model', 'Unlocked', 'Cores', 'Price','Info');
         $orderGPUBy = array('Model', '3DMark', 'Cores', 'Price');//include this in the GPUY array
         $ordercpu = "Model";
@@ -68,19 +69,12 @@ alert(info);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //return new PDO("mysql:host=$host;dbname=$dbname", $username, $password);//For some reason this line from Jasons exampled isnt working for me
         //////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////
-        //$sql = " SELECT * FROM device ORDER BY deviceName";//This will sort them alphabetically by the device name add DESC to reverse the order
-        //$ordercpu = 'deviceName';
         $sql =  'SELECT * FROM Processors WHERE Model LIKE "'.$model.'%" AND Unlocked LIKE "'.$unlocked.'%" AND Cores LIKE "'.$cores.'%" ORDER BY ' .$ordercpu;
-        // $sql =  'SELECT * FROM device WHERE deviceName LIKE "'.$name.'%" ORDER BY ' .$ordercpu;//Works
-        ////$sql = 'SELECT * FROM device WHERE deviceName = "HTC Vive" ORDER BY ' .$ordercpu;
-        //$sql = 'SELECT * FROM device WHERE deviceName = '.$name.' ORDER BY ' .$ordercpu;
 
         $stmt = $dbConn -> prepare ($sql);
         $stmt -> execute (  array ( ':id' => '1')  );
         if ($stmt->rowCount() > 0) {
             //Creates a table
-            //$table_str.='<div>';
             $table_str.= '<div class="column">';
             $table_str.='CPU';
             $table_str.='<table>';
@@ -90,13 +84,10 @@ alert(info);
             $table_str.='<th>'. '<a href="?orderCPUBy=Price">Price</a>'.'</th>';
 
         while ($row = $stmt -> fetch())  {
-            //echo  $row['deviceName'] . ", " . $row['deviceType'] . ', ' . $row['price'] . ", " . $row['status']. "</br>";
             $table_str.='<tr>';
             $table_str.='<td>'.$row['Model'].'</td>'.'<td>'.$row['Unlocked'].'</td>'.'<td>'.$row['Cores'].'</td>'.'<td>'.$row['Price'].'</td>';
-            $table_str.= '<td><form action="welcome.php" method="post"><input type="submit"" value = "Add to Cart"></td>';//Adds the button to add the item to the users sessions, I have not yet implemented the session
-            //$table_str.= '<td><form action="welcome.php" method="post"><input type="submit"" value = "info" style="background-color:blue"></td>';
+            $table_str.= '<td><form action="" method="post"><input type="submit"" value = "Add to Cart"></td>';//Adds the button to add the item to the users sessions, I have not yet implemented the session
             $table_str.='<td>';
-            //$table_str.='<input type="button" onclick=alert("';
             $table_str.='<input type="button" onclick="myFunction(';
             $table_str.="'";
             $table_str.=$row['Info'];//This was difficult for some reason, we need to add the column to this and then pull that instead of model
@@ -105,18 +96,13 @@ alert(info);
             $table_str.='</tr>';
         }
         $table_str.='</table>';
-        $table_str2.='</div>';
-        //This the attemt to build the array with the graphics cards
+        $table_str.-'</div>';
+        //$table_str2.='</div>';
         $sql =  'SELECT * FROM GPU WHERE Model LIKE "'.$model.'%" ORDER BY ' .$ordergpu;
-        // $sql =  'SELECT * FROM device WHERE deviceName LIKE "'.$name.'%" ORDER BY ' .$ordercpu;//Works
-        ////$sql = 'SELECT * FROM device WHERE deviceName = "HTC Vive" ORDER BY ' .$ordercpu;
-        //$sql = 'SELECT * FROM device WHERE deviceName = '.$name.' ORDER BY ' .$ordercpu;
-
         $stmt = $dbConn -> prepare ($sql);
         $stmt -> execute (  array ( ':id' => '1')  );
         if ($stmt->rowCount() > 0) {
-            //Creates a table
-           // $table_str.='<div>';
+            //Creates the table headers
             $table_str2.='<div class="column">';
             $table_str2.= 'Graphics cards';
             $table_str2.='<table>';
@@ -126,34 +112,66 @@ alert(info);
             $table_str2.='<th>'. '<a href="?orderGPUBy=Price">Price</a>'.'</th>';
 
         while ($row = $stmt -> fetch())  {
-            //echo  $row['deviceName'] . ", " . $row['deviceType'] . ', ' . $row['price'] . ", " . $row['status']. "</br>";
+            //Creates the table rows
             $table_str2.='<tr>';
             $table_str2.='<td>'.$row['Model'].'</td>'.'<td>'.$row['3DMark'].'</td>'.'<td>'.$row['Cores'].'</td>'.'<td>'.$row['Price'].'</td>';
+            $table_str2.= '<td><form action="" method="post"><input type="submit"" value = "Add to Cart"></td>';//Adds the button to add the item to the users sessions, I have not yet implemented the session
+            $table_str2.='<td>';
+            $table_str2.='<input type="button" onclick="myFunction(';
+            $table_str2.="'";
+            $table_str2.=$row['Info'];//This was difficult for some reason, we need to add the column to this and then pull that instead of model
+            $table_str2.="'".')" value="Info" />';
+            $table_str2.='</td>';
             $table_str2.='</tr>';
         }
         $table_str2.='</table>';
         $table_str2.='</div>';
-        //This is the end of the attempt to build the array with the graphics cards
-        //$table_str.='</div>';
+        }
+        
+        
+        
+        
+        $table_str3.='</div>';
+        $sql =  'SELECT * FROM GPU WHERE Model LIKE "'.$model.'%" ORDER BY ' .$ordergpu;
+        $stmt = $dbConn -> prepare ($sql);
+        $stmt -> execute (  array ( ':id' => '1')  );
+        if ($stmt->rowCount() > 0) {
+            //Creates a table headers
+            $table_str3.='<div class="column">';
+            $table_str3.='Graphics cards';
+            $table_str3.='<table>';
+            $table_str3.='<th>'.  '<a href="?orderGPUBy=Model">Model</a>'.'</th>';
+            $table_str3.='<th>'. '<a href="?orderGPUBy=3DMark">3DMark</a>'.'</th>';
+            $table_str3.='<th>'. '<a href="?orderGPUBy=Cores">Cores</a>'.'</th>';
+            $table_str3.='<th>'. '<a href="?orderGPUBy=Price">Price</a>'.'</th>';
+
+        while ($row = $stmt -> fetch())  {
+            //fills the table rows
+            $table_str3.='<tr>';
+            $table_str3.='<td>'.$row['Model'].'</td>'.'<td>'.$row['3DMark'].'</td>'.'<td>'.$row['Cores'].'</td>'.'<td>'.$row['Price'].'</td>';
+            $table_str3.= '<td><form action="" method="post"><input type="submit"" value = "Add to Cart"></td>';//Adds the button to add the item to the users sessions, I have not yet implemented the session
+            $table_str3.='<td>';
+            $table_str3.='<input type="button" onclick="myFunction(';
+            $table_str3.="'";
+            $table_str3.=$row['Info'];//This was difficult for some reason, we need to add the column to this and then pull that instead of model
+            $table_str3.="'".')" value="Info" />';
+            $table_str3.='</td>';
+            $table_str3.='</tr>';
+        }
+        $table_str3.='</table>';
+        //$table_str3.='</div>';
         echo '<div class="row">';
-       // echo '<div class="column">';
         echo $table_str;
-        //echo '</div>';
-        //echo '<div class="column">';
         echo $table_str2;
-        //echo '</div>';
+        echo $table_str3;
 
         echo '</div>';
-        //echo $table_str;
         }
         }
         else {
         echo "No data found";
         }
         ?>
-        <form>
-            <!--<input type="submit" value="Spin!"/>-->
-        </form>
         </div>
     </body>
 </html>
